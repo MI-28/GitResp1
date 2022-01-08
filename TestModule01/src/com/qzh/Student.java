bf78aefe1822adbbc3fa589d21b91274d4d66c7e
qin <957250119@qq.com>
2022-01-08 11:26:25 +0800

    修改了Student类

--- TestModule01/src/com/qzh/Student.java
+++ TestModule01/src/com/qzh/Student.java
@@ -1,8 +1,7 @@
 package com.qzh;
 
 public class Student {
-    public static void main(String[] args){
-        System.out.println("woc");
-        System.out.println("SSM");
-    }
+    private String sno;
+    private String sname;
+    private String sex;
 }
